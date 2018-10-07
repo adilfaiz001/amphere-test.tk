1b1dbbf28406195413a518ecc9f0750b84943edc
Adil Faiz <adilfaiz01@live.com>
2018-10-07 13:42:02 +0530

    Coupon Worker

--- util/CouponWorker.js
+++ util/CouponWorker.js
@@ -53,9 +53,8 @@ exports.generateSelfCoupon = function(params)
 
     return new Promise((resolve,reject)=>{
         let cid = generateCID();
-        
         CouponsData.ref().child('coupons').orderByChild('code').equalTo(coupon).limitToFirst(1).once('value',(couponres)=>{
-            console.log(couponres);
+            console.log(couponres.val());
             if(couponres.val() === null)
             {
                 CouponsData.ref('coupons/cid-' + cid).set({
