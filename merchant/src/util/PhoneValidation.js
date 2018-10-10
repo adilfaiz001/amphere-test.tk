f14a006f8c438e497bcf78cf4a3b2a227787e4bb
adilfaiz001 <adilfaiz01@live.com>
2018-10-10 20:31:55 +0530

    merchant coupon worker

--- merchant/src/util/PhoneValidation.js
+++ merchant/src/util/PhoneValidation.js
@@ -1,4 +1,3 @@
-
 exports.ValidatePhone = (user) => {    
     return new Promise((resolve,reject)=>{
         if(user !== "")
@@ -17,17 +16,6 @@ exports.ValidatePhone = (user) => {
                     console.log(response);
                     if(response.state === 'SUCCESS')
                     {
-                        if(response.CouponValid === true)
-                        {
-                            resolve({
-                                "valid":true,
-                                "username":response.username,
-                                "promoValid":true,
-                                "promoCode":response.Coupon,
-                                "promoAmount":response.Amount,
-                                user
-                            })
-                        }
                         resolve({
                             "valid":true,
                             "username":response.username,
@@ -41,6 +29,42 @@ exports.ValidatePhone = (user) => {
                             'valid':false
                         });
                     }
+                }
+            }
+        }
+    });
+}
+
+
+exports.ValidateCoupon = function (user) {
+    return new Promise((resolve,reject)=>{
+        const request = new XMLHttpRequest();
+        const url = `user=${encodeURI(user)}`;
+
+        request.open('POST',`/validateCoupon?${url}`,true);
+        request.send();
+
+        request.onreadystatechange = (event) => {
+            if(request.readyState === 4 && request.status === 200)
+            {
+                let response = JSON.parse(request.response);
+                if(response.state === 'SUCCESS')
+                {
+                    if(response.CouponValid === true)
+                    {
+                        resolve({
+                            "valid":true,
+                            "promoValid":true,
+                            "promoCode":response.Coupon,
+                            "promoAmount":response.Amount,
+                        });
+                    }
+                }
+                else
+                {
+                    resolve({
+                        'valid':false
+                    });
                 }
             }
         }
