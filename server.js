7b15ae7fb2fd8389960a1600d6936e50949bff59
adilfaiz001 <adilfaiz01@live.com>
2018-10-19 17:48:13 +0530

    signup flash

--- server.js
+++ server.js
@@ -85,8 +85,7 @@ homepage.use(function(req,res,next){
 //---------------------------- HOMEPAGE -----------------------------//
 
 homepage.get('/', (req,res)=> {
-    console.log(req.flash('success'));
-    res.render('index', { title: 'Home | Amphere Solutions', expressFlass: req.flash('success') });
+    res.render('index', { title: 'Home | Amphere Solutions'});
 });
 homepage.get('/signup', (req,res)=> {
     res.render('signup', { title: 'Sign Up | Amphere Solutions' });
