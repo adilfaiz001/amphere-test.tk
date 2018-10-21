7d5aed03b0b18bb0ed680be8bf0a60a0652b0280
adilfaiz001 <adilfaiz01@live.com>
2018-10-22 02:21:28 +0530

    cont

--- account/src/components/Adder.jsx
+++ account/src/components/Adder.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './css/Adder.css';
+import $ from 'jquery';
 
 class Adder extends Component {
+    componentDidMount(){
+        $('#add-email').on('click',function(){
+            $('#add-email').addClass('input-hidden');
+            $('.email-input').removeClass('input-hidden');
+        });
+    }
     render() {
         return (
-            <div>
+            <div className="container">
                 <div className="email-add">
-                    <p><a href="/addEmail">Add email to your account</a></p>
+                    <p><a id="add-email">Add email to your account</a></p>
                 </div>
-                <div className="email-input">
+                <div className="email-input input-hidden">
                     <input type="text" className="email-input" placeholder="Add your email"/>
                     <input type="submit" className="email-submit" name="submit-email" id="submit-btn" placeholder="add"/>
                 </div>
