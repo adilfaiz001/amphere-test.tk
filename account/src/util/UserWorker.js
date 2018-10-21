72f8a33824a2ba3c5a82b8d58b3e9ae8302a2a50
adilfaiz001 <adilfaiz01@live.com>
2018-10-22 01:39:45 +0530

    cont

--- account/src/util/UserWorker.js
+++ account/src/util/UserWorker.js
@@ -4,20 +4,20 @@ exports.ResendEmail = (phone) =>{
     })
 }
 
-exports.CheckForEmail = (phone) =>{
+exports.CheckForEmail = (uid) =>{
     const UserData = require('./Database').firebase.database();
     return new Promise((resolve,reject)=>{
-    UserData.ref().child('users').orderByChild('phone').equalTo(phone).limitToFirst(1).once('child_added',(userch,err)=>{
-            console.log(userch.val());
-            if(userch.val() !== null)
-            {
-                var addEmail = (UserData.ref('users/user-' + userch.child('uid').val() + 'email').val() !== null )
-                console.log(addEmail);   
-            }
-            resolve();
-        });
-        }).catch((err) => {
+        UserData.ref().child('users').orderByChild('uid').equalTo(uid).limitToFirst(1).once('child_added',(userch,err)=>{
+                console.log(userch.val());
+                if(userch.val() !== null)
+                {
+                    var addEmail = (UserData.ref('users/user-' + userch.child('uid').val() + 'email').val() !== null )
+                    console.log(addEmail);   
+                }
+                resolve();
+            });
+    }).catch((err) => {
             console.log("HERE IN ERROR");
             console.log(err);
-        });
+    });
 }
