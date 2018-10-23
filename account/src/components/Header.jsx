cf985c481c95ac7aa17525ae8e52aeb058bc859f
adilfaiz001 <adilfaiz01@live.com>
2018-10-24 05:18:39 +0530

    email verify block

--- account/src/components/Header.jsx
+++ account/src/components/Header.jsx
@@ -196,7 +196,7 @@ class Header extends Component {
                                                         <div className="email-send-verify">
                                                             <p>Send Email for verification</p>
                                                             <span onClick={this.SendEmail}>
-                                                                <AwesomeButton size="Large" type="primary" color="teal" >Send Email</AwesomeButton>
+                                                                <AwesomeButton size="large" type="primary" progress="true" >Send Email</AwesomeButton>
                                                             </span>
                                                         </div>
                                                         :
