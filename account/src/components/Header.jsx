b1a1b6a385249bc2f4d17b8e2fb2ff53a6792e82
adilfaiz001 <adilfaiz01@live.com>
2018-10-26 19:48:14 +0530

    checkmark

--- account/src/components/Header.jsx
+++ account/src/components/Header.jsx
@@ -227,7 +227,7 @@ class Header extends Component {
                                                                         <svg class="checkemail" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
                                                                     </div>
                                                                     :
-                                                                    <div className="email-final">
+                                                                    <div className="email-sent">
                                                                         <p>Email Sent for verification</p>
                                                                     </div>                        
                                                             }
