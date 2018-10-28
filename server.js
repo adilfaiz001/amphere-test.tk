2a83d5c3c7d416acb4b271c2a8c0deb93e4f21a7
adilfaiz001 <adilfaiz01@live.com>
2018-10-29 01:24:59 +0530

    email css

--- server.js
+++ server.js
@@ -229,7 +229,7 @@ homepage.get('/confirm_email/:hash',(req,res)=>{
         SignupWorker.ConfirmEmail({
             "UserIdHash" : UserIdHash
         }).then((_res)=>{
-            console.log(res);
+            console.log(_res);
             if(_res.success)
             {
                 res.render('confirm_mail',{title: 'Confirm Mail | Amphere Solutions' , success : true});
