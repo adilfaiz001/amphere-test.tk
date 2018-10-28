f9c293f45ff2d366e43377360abbf5de31688abc
adilfaiz001 <adilfaiz01@live.com>
2018-10-29 01:19:50 +0530

    email css

--- util/SignupWorker.js
+++ util/SignupWorker.js
@@ -230,7 +230,7 @@ exports.ConfirmEmail = (params) => {
     return new Promise((resolve,reject)=>{
         UsersData.ref().child('users').orderByChild('userIdToken').equalTo(UserIdHash).limitToFirst(1).once('value',(userch)=>{
             console.log(userch.val());
-            if(userch.val() !== null)
+            if(userch.child('userIdToken').val() !== null)
             {
                 UsersData.ref('users/user-' + userch.child('uid').val()).update({
                     "emailVerify":true,
